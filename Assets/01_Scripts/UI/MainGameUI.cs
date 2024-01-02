809ba589180c63f512a50ac1ba37cc3079ae4399
9mean3 <gminseong114@gmail.com>
2024-01-02 20:06:32 +0900

    crystal.

--- Assets/01_Scripts/UI/MainGameUI.cs
+++ Assets/01_Scripts/UI/MainGameUI.cs
@@ -10,65 +10,12 @@ using UnityEngine.SceneManagement;
 public class MainGameUI : MonoBehaviour
 {
     [SerializeField] private RectTransform _startButton;
-    [SerializeField] private RectTransform _restartButton;
 
-    [SerializeField] private RectTransform _pausePanel;
-    [SerializeField] private GameObject _otherButtonControlPanel;
-
-    public bool testIsPause = false;
-
-    private void Start()
+    public void SetStartButton(bool isStart)
     {
-        _pausePanel.transform.Find("StageName").GetComponent<TextMeshProUGUI>().text = SceneManager.GetActiveScene().name; 
-    }
-
-    public void SetStartButton(bool isStartButtonDown)
-    {
-        RectTransform inside, outside;
-        if (isStartButtonDown)
-        {
-            inside = _restartButton;
-            outside = _startButton;
-        }
+        if (isStart)
+            _startButton.DOAnchorPosY(_startButton.sizeDelta.y, 0.2f).SetEase(Ease.OutQuad);
         else
-        {
-            inside = _startButton;
-            outside = _restartButton;
-        }
-
-        Sequence seq = DOTween.Sequence();
-        seq.Append(outside.DOAnchorPosY(outside.sizeDelta.y, 0.2f));
-        seq.Append(inside.DOAnchorPosY(0, 0.2f));
-    }
-
-    public void PauseToggleButton()
-    {
-        testIsPause = !testIsPause;
-        SetPausePanel(testIsPause);
-    }
-
-    public void PauseFalseButton()
-    {
-        testIsPause = false;
-        SetPausePanel(testIsPause);
-    }
-
-    public void SetPausePanel(bool isPause)
-    {
-        float targetY;
-
-        DOTween.Kill(_pausePanel);
-
-        _otherButtonControlPanel.SetActive(isPause);
-        if (isPause)
-        {
-            targetY = 0;
-        }
-        else
-        {
-            targetY = Screen.height * 1.2f;
-        }
-
-        _pausePanel.DOAnchorPosY(targetY, isPause ? 0.85f : 0.5f).SetEase(Ease.OutBounce);
+            _startButton.DOAnchorPosY(0, 0.2f).SetEase(Ease.OutQuad); 
     }
 }
