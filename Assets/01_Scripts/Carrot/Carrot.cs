b70a680e3d93d7a9dea046939af4f157f6b4fe77
dabin0519 <dadabin0519@naver.com>
2024-01-02 15:53:01 +0900

    key

--- Assets/01_Scripts/Carrot/Carrot.cs
+++ Assets/01_Scripts/Carrot/Carrot.cs
@@ -9,7 +9,7 @@ public abstract class Carrot : MonoBehaviour
     protected PlayerHP _playerHP;
     protected Collider2D _collider;
 
-    protected bool _isNotDestroy;
+    protected bool _isNotDestroy = false;
 
     protected virtual void Awake()
     {
@@ -23,7 +23,7 @@ public abstract class Carrot : MonoBehaviour
 
     protected virtual void Update()
     {
-        if(_batchCheck.BatchClearPro)
+        if(_batchCheck != null && _batchCheck.BatchClearPro)
         {
             _collider.enabled = true;
         }
--- Assets/01_Scripts/Carrot/EndFlag.cs
+++ Assets/01_Scripts/Carrot/EndFlag.cs
@@ -6,9 +6,12 @@ using UnityEngine.Events;
 public class EndFlag : Carrot
 {
     public UnityEvent EndEvent;
+    public bool IsKey;
 
     protected override void CarrotAbility()
     {
+        if (IsKey)
+            return;
         _player.Active = false;
         EndEvent?.Invoke();
     }
