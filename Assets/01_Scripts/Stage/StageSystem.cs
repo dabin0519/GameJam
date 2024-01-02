4bc811cd77a1ad18b2c1897da60c6f2dc72b2854
JangWheesung <gameplay1qaz@gmail.com>
2024-01-02 10:53:21 +0900

    스테이지, 별 로드세이브

--- Assets/01_Scripts/Stage/StageSystem.cs
+++ Assets/01_Scripts/Stage/StageSystem.cs
@@ -8,7 +8,7 @@ using TMPro;
 using DG.Tweening;
 using UnityEngine.SceneManagement;
 
-public class StageSystem : MonoBehaviour
+public class StageSystem : MonoBehaviour, ISaveManager
 {
     public static StageSystem Instance;
 
@@ -29,6 +29,8 @@ public class StageSystem : MonoBehaviour
     [Header("프리팹")]
     [SerializeField] private Image starImage;
 
+    private GameData gameData;
+
     private TextMeshProUGUI timeText;
     private RectTransform starPanel;
     private Button stageBtn;
@@ -41,7 +43,18 @@ public class StageSystem : MonoBehaviour
     private void Awake()
     {
         Instance = this;
+    }
 
+    private void Update()
+    {
+        if (IsPlay)
+        {
+            currentPlayTime += Time.deltaTime;
+        }
+    }
+
+    private void LoadStage()
+    {
         Instantiate(stageData[currentStage].map); //맵 생성
 
         foreach (PuzzleData puzzleData in stageData[currentStage].puzzleDatas) //퍼즐 생성
@@ -53,19 +66,11 @@ public class StageSystem : MonoBehaviour
 
         playBtn.onClick.AddListener(PlayBtn);//실행버튼 함수넣고
         OnClearEvt += GameClear;//클리어시 창 뜨게
-        
+
         timeText = clearPanel.Find("MainPanel").Find("TimeText").GetComponent<TextMeshProUGUI>();//분초로
         starPanel = clearPanel.Find("MainPanel").Find("StarPanel").GetComponent<RectTransform>();//별 달아놓는 곳
         stageBtn = clearPanel.Find("MainPanel").Find("StageBtn").GetComponent<Button>();//스테이지 돌아가기
         nextBtn = clearPanel.Find("MainPanel").Find("NextBtn").GetComponent<Button>();//다음 스테이지
-    }
-
-    private void Update()
-    {
-        if (IsPlay)
-        {
-            currentPlayTime += Time.deltaTime;
-        }
     }
 
     public void PlusStar()
@@ -106,14 +111,31 @@ public class StageSystem : MonoBehaviour
 
     private void NextGame(int nextStage) //스테이지 선택 시 메계변수X
     {
-        //현재 스테이지에서 먹은 별개수 저장하고
-        //현재 스테이지를 nextStage로 설정 이후
+        SaveData(ref gameData);
         SceneManager.LoadScene("");//씬이동
     }
 
     private void GameOut()
     {
-        //현재 스테이지에서 먹은 별개수 저장하고
+        SaveData(ref gameData);
         SceneManager.LoadScene("");//씬이동
+    }
+
+    public void LoadData(GameData data) //스테이지 불러오기(Start에서 호출)
+    {
+        gameData = data;
+        currentStage = data.stage;
+        LoadStage();
+    }
+
+    public void SaveData(ref GameData data) //다음 스테이지, 별 저장
+    {
+        //현재 스테이지에서 먹은 별개수 저장하고
+        if (currentStar > data.star[currentStage])
+        {
+            data.star[currentStage] = currentStar;
+        }
+        //현재 스테이지를 nextStage로 설정
+        data.stage = currentStage + 1;
     }
 }
