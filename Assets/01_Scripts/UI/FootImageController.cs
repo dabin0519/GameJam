1c9fb4805ee96d0f0d5c594e5c0677af185f40aa
9mean3 <gminseong114@gmail.com>
2024-01-03 20:15:09 +0900

    .

---
+++ Assets/01_Scripts/UI/FootImageController.cs
@@ -0,0 +1,27 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootImageController : MonoBehaviour
+{
+    [SerializeField] private GameObject _image;
+
+    private PlayerController _playerController;
+
+    private void Start()
+    {
+        _playerController = FindObjectOfType<PlayerController>();
+    }
+
+    private void Update()
+    {
+        SetFootImage();
+        Debug.Log(_playerController.MoveCnt);
+    }
+
+    public void SetFootImage()
+    {
+        
+    }
+}
